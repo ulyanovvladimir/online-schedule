3c06057112af6e2854d0ad55771d1d9bc0453409
mirenva <mirenva@mail.ru>
2015-06-25 15:46:52 +0900

    добавлен ClientAPI

---
+++ app/controllers/ClientAPI.java
@@ -0,0 +1,39 @@
+package controllers;
+
+import models.Lesson;
+import play.*;
+import play.mvc.*;
+import views.html.*;
+import play.data.*;
+import java.io.*;
+import java.util.*;
+import play.db.ebean.*;
+import play.libs.Json;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+
+public class ClientAPI extends Controller {
+  
+  public static Result findLessons(String groupNumber, String day, String hours, String lecture, String teacher, String room) {
+  
+    List<Lesson> lessonList = Lesson.find.where()
+		.ilike("groupNumber", "%" + groupNumber + "%")
+		.ilike("day", "%" + day + "%")
+		.ilike("hours", "%" + hours + "%")
+		.ilike("lecture", "%" + lecture + "%")
+		.ilike("teacher", "%" + teacher + "%")
+		.ilike("room", "%" + room + "%")
+		.findList();
+
+	ObjectNode result = Json.newObject();
+    JsonNode lessonsListJson = Json.toJson(lessonList);
+	
+	result.put("objects", lessonsListJson);
+	
+	return ok(result);	
+  }
+  
+  
+  }
