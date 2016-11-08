c6355ef08855b83a8845ad55c0cd80f690b9e7f5
Vladimir Ulyanov <dedushka.lenin@gmail.com>
2016-11-08 23:06:04 +0800

    шаблон main

--- app/controllers/App.scala
+++ app/controllers/App.scala
@@ -6,8 +6,8 @@ import play.api.mvc.{Action, Controller}
 /**
   * @author Vladimir Ulyanov
   */
-object App extends Controller{
-  def index = Action {
+object App extends Controller {
+  def index() = Action {
     //todo filter
     val lessonList = Lesson.all()
     Ok(views.html.index.render(lessonList))
