9df1ae79107e2226614d1c2473a9006e3e0e9d48
Vladimir Ulyanov <dedushka.lenin@gmail.com>
2017-03-03 20:16:47 +0800

    API для получения списка преподавателей №6Э

--- app/controllers/API.scala
+++ app/controllers/API.scala
@@ -1,8 +1,20 @@
 package controllers
 
-/**
-  * Created by User on 03.03.2017.
-  */
-class API {
+import play.api.libs.json.Json
+import play.api.mvc.{Action, Controller}
 
+
+/**
+  * @author Vladimir Ulyanov
+  */
+object API extends Controller {
+  /**
+    *
+    * @return JSON array of instructors in alphabetical order
+    */
+  def instructors() = Action {
+    Ok(Json.toJson(App.allInstructors))
+  }
+
+  def lessons() = TODO
 }
